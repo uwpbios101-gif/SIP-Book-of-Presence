8bfe82310f46a50d0f4e175b97243bda1d9a6499
selassiefest-web <selassiefest@gmail.com>
2026-07-11 13:30:09 -0500

    version 6.1

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,25 @@
-const CACHE = 'sip-book-of-presence-v6-complete-24-drinks';
+const CACHE = 'sip-book-of-presence-v6-1-batch-1-photography';
 const CORE = [
   './',
   './index.html',
-  './assets/css/styles.css?v=6.0.0',
+  './assets/css/styles.css?v=6.1.0',
   './assets/js/app.js?v=6.0.0',
   './assets/img/sip-logo.png',
   './assets/img/promotion-ambient.jpg',
   './assets/img/girls-night-ambient.jpg',
   './assets/img/family-ambient.jpg',
   './assets/data/menu.json',
-  './manifest.webmanifest?v=6.0.0'
+  './assets/img/01-presence.png',
+  './assets/img/02-clarity.png',
+  './assets/img/03-legacy.png',
+  './assets/img/04-renaissance.png',
+  './assets/img/05-the-sovereign.png',
+  './assets/img/06-midnight-gold.png',
+  './assets/img/07-quiet-authority.png',
+  './assets/img/08-afterglow.png',
+  './assets/img/09-grounded.png',
+  './assets/img/10-provision.png',
+  './manifest.webmanifest?v=6.1.0'
 ];
 
 self.addEventListener('install', event => {
